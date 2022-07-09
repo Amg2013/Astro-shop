9a4a5251370369a0151ccf98d26ec82fb860d6ff
Shehab-adel <shoopa5050@gmail.com>
2022-07-09 15:49:18 +0200

    added sign up screen and its dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:shooping_app/routes/app_routes.dart';
-
 void main() {
   runApp(const MyApp());
 }
