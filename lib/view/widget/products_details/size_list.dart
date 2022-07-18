4111f04de053f388e56fdf42d1479a57682bd49b
Shehab-adel <shoopa5050@gmail.com>
2022-07-18 21:17:42 +0200

    added clothes info to products_details.dart

---
+++ lib/view/widget/products_details/size_list.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shooping_app/utils/theme.dart';
+
+class SizeList extends StatefulWidget {
+  const SizeList({Key? key}) : super(key: key);
+
+  @override
+  State<SizeList> createState() => _SizeListState();
+}
+
+class _SizeListState extends State<SizeList> {
+  final List<String> sizeList = ['S', 'M', 'L', 'XL', 'XXL'];
+  int currentSelected = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+      child: ListView.separated(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                currentSelected = index;
+                setState(() {});
+              },
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                decoration: BoxDecoration(
+                  color: Get.isDarkMode
+                      ? currentSelected == index
+                          ? pinkClr.withOpacity(0.4)
+                          : Colors.black
+                      : currentSelected == index
+                          ? mainColor.withOpacity(0.4)
+                          : Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  border:
+                      Border.all(color: Colors.grey.withOpacity(0.4), width: 2),
+                ),
+                child: Text(
+                  sizeList[index],
+                  style: TextStyle(
+                      color: Get.isDarkMode ? Colors.white : Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            );
+          },
+          separatorBuilder: (context, index) {
+            return SizedBox(
+              width: 10,
+            );
+          },
+          itemCount: sizeList.length),
+    );
+  }
+}
