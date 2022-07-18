dee7fda0d2fb378bd7cddd7ddf265dad8c69e5f7
Shehab-adel <shoopa5050@gmail.com>
2022-07-18 15:14:49 +0200

    added color picker in image slider to product details

--- lib/view/widget/products_details/image_slider.dart
+++ lib/view/widget/products_details/image_slider.dart
@@ -2,6 +2,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shooping_app/utils/theme.dart';
+import 'package:shooping_app/view/widget/products_details/color_picker.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class ImageSlider extends StatefulWidget {
@@ -17,6 +18,15 @@ class _ImageSliderState extends State<ImageSlider> {
   CarouselController carouselController = CarouselController();
 
   int currentPage = 0;
+  int currentColor = 0;
+
+  final List<Color> colorSelectedList = [
+    kCOlor1,
+    kCOlor2,
+    kCOlor3,
+    kCOlor4,
+    kCOlor5,
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -61,6 +71,35 @@ class _ImageSliderState extends State<ImageSlider> {
                   activeDotColor: Get.isDarkMode ? pinkClr : mainColor,
                   dotColor: Colors.black),
             )),
+        Positioned(
+            bottom: 30,
+            right: 30,
+            child: Container(
+              height: 210,
+              width: 50,
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.3),
+                  borderRadius: BorderRadius.circular(30)),
+              child: ListView.separated(
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        currentColor = index;
+                        setState(() {});
+                      },
+                      child: ColorPicker(
+                          outerBorder: currentColor == index,
+                          color: colorSelectedList[index]),
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return SizedBox(
+                      height: 3,
+                    );
+                  },
+                  itemCount: colorSelectedList.length),
+            ))
       ],
     );
   }
