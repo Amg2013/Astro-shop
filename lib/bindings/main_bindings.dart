0ec44d08931d18fe6ceeb38262834d1cc79443f5
Shehab-adel <shoopa5050@gmail.com>
2022-07-22 11:49:10 +0200

    added dark mode, log out widget in settings screen

--- lib/bindings/main_bindings.dart
+++ lib/bindings/main_bindings.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
 import 'package:shooping_app/controller/main_controller.dart';
+import 'package:shooping_app/controller/settings_controller.dart';
 
 class MainBinding extends Bindings {
   @override
   void dependencies() {
     // TODO: implement dependencies
     Get.put(MainController());
+    Get.put(SettingsController());
   }
 }
---
+++ lib/view/widget/settings/dark_modeWidget.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shooping_app/controller/theme_controller.dart';
+import 'package:shooping_app/utils/theme.dart';
+import 'package:shooping_app/view/widget/text_utils.dart';
+
+import '../../../controller/settings_controller.dart';
+
+class DarkModeWidget extends StatelessWidget {
+  DarkModeWidget({Key? key}) : super(key: key);
+
+  final settingsController = Get.find<SettingsController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          buildIconWidget(),
+          Switch(
+            value: settingsController.switchValue.value,
+            onChanged: (value) {
+              ThemeController().changeTheme();
+              settingsController.switchValue.value = value;
+            },
+            activeTrackColor: Get.isDarkMode ? pinkClr : mainColor,
+            activeColor: Get.isDarkMode ? pinkClr : mainColor,
+          )
+        ],
+      );
+    });
+  }
+
+  Widget buildIconWidget() {
+    return Material(
+      color: Colors.transparent,
+      child: Row(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(6),
+            decoration: const BoxDecoration(
+                shape: BoxShape.circle, color: darkSettings),
+            child: const Icon(
+              Icons.dark_mode,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          TextUtils(
+              fontSize: 22,
+              color: Get.isDarkMode ? Colors.white : Colors.black,
+              fontWeight: FontWeight.bold,
+              underLine: TextDecoration.none,
+              text: 'Dark Mode')
+        ],
+      ),
+    );
+  }
+}
